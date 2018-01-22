c43ab0a126e09d9aa8cff4435f6e28521707483e
Boram Yoon <boram@me.com>
2018-01-22 11:17:09 -0800

    Set up Enzyme
    * Remove previous Jest snapshot test
    * Do not install jest-serialize-enzyme due to a broken dependency
    * Therefore do not perform snapshot tests using Enzyme

---
+++ src/test/env.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { shallow, mount } from 'enzyme'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+global.React = React
+global.shallow = shallow
+global.mount = mount
